5cf31a45ba5caa1211d9612605e166148b57d3b3
Dyanesh Waran <dyanesh16@outlook.com>
2018-08-08 19:18:31 +0530

    Welcome, guys.

---
+++ Welcome.py
@@ -0,0 +1,3 @@
+print("Welcome to our Repository.")
+print("Please make note of the URL of our GitHub repository in order to clone it on your local PC.")
+print("Looking forward to work with you guys.")
